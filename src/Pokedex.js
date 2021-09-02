ed586613c16a6925db383c6d4a32df136a734a9b
Ariane Ueti <arianeueti@gmail.com>
2021-09-02 20:57:11 -0300

    Exercício 1

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,10 +2,37 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor() {
+        super()
+        this.nextPokemon = this.nextPokemon.bind(this);
+        this.comparePokemon = this.comparePokemon.bind(this);
+
+        this.state = {
+            thisPokemon: 1,
+        }
+    }
+
+    comparePokemon(){
+        if ( this.state.thisPokemon < 8 ) {
+            this.nextPokemon();
+        } else {
+            this.setState(() => ({
+                thisPokemon: 0,
+            }));
+        }
+    }
+
+    nextPokemon(){
+        this.setState((prevState, _prop) => ({
+            thisPokemon: prevState.thisPokemon + 1,
+        }));
+    }
     render() {
+        const { pokemons } = this.props;
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                { pokemons.filter((pokemon, index) => pokemons[index] === pokemons[this.state.thisPokemon]).map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} />) }
+                <button onClick={ this.comparePokemon }>Next !</button>
             </div>
         );
     }
